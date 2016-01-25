b711052c32c59ff0317fd1417d70c6d88ebdbb7c
jwdomingo <johnwill.domingo@gmail.com>
2016-01-25 15:28:59 -0800

    add init method, addMessage updated fetch to take a callback

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,13 +1,17 @@
 // YOUR CODE HERE:
 var App = function(){
   this.server = "https://api.parse.com/1/classes/chatterbox";
+  this.messages = [];
 };
 
-App.prototype.init = function() {
-  // body...
-  // load messages
-  // set username?
-  // intro animation?
+App.prototype.init = function(){
+  var self = this;
+  this.fetch(function(data){
+    self.messages = data.results;
+    _.each( data.results,
+      function(msg){self.addMessage(msg);
+    });
+  });
 };
 
 App.prototype.send = function(message) {
@@ -25,13 +29,16 @@ App.prototype.send = function(message) {
   });
 };
 
-App.prototype.fetch = function() {
+App.prototype.fetch = function(successCallBack) {
   return $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
     contentType: 'application/json',
     success: function (data) {
-      console.log('chatterbox: fetched messsages');
+      if(successCallBack){
+        successCallBack(data);
+      }
+      return data;
     },
     error: function (data) {
       console.error('chatterbox: Failed to fetch messages');
@@ -43,4 +50,11 @@ App.prototype.clearMessages = function(){
   $('#chats').children().remove();
 };
 
+App.prototype.addMessage = function(message) {
+  $('#chats').append($("<div>"+message.text+"</div>"));
+};
+
+//$('form').submit(var messageText = $('#chatBox').value();)
+
 var app = new App();
+app.init();
