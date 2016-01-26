2118e5c96a9b554a69f1f93dcd793ca28557c947
jwdomingo <johnwill.domingo@gmail.com>
2016-01-25 16:49:19 -0800

    Modify init, fix send, add button click event listener

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,25 +2,40 @@
 var App = function(){
   this.server = "https://api.parse.com/1/classes/chatterbox";
   this.messages = [];
+  this.rooms = {};
 };
 
 App.prototype.init = function(){
   var self = this;
   this.fetch(function(data){
+    roomKey = {};
     self.messages = data.results;
+    
     _.each( data.results,
-      function(msg){self.addMessage(msg);
+      function(msg){
+        self.addMessage(msg);
+        roomKey[msg.roomname] = roomKey[msg.roomname] || [];
+        roomKey[msg.roomname].push(msg);
+    });
+
+    self.rooms = roomKey;
+
+    _.each(self.rooms,function(val,key){
+      self.addRoom(key);
     });
   });
 };
 
-App.prototype.send = function(message) {
+App.prototype.send = function(message, _successCallBack) {
   return $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'POST',
     data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
+      if(_successCallBack){
+        _successCallBack(data);
+      }
       console.log('chatterbox: Message sent');
     },
     error: function (data) {
@@ -29,14 +44,14 @@ App.prototype.send = function(message) {
   });
 };
 
-App.prototype.fetch = function(successCallBack) {
+App.prototype.fetch = function(_successCallBack) {
   return $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
     contentType: 'application/json',
     success: function (data) {
-      if(successCallBack){
-        successCallBack(data);
+      if(_successCallBack){
+        _successCallBack(data);
       }
       return data;
     },
@@ -51,10 +66,27 @@ App.prototype.clearMessages = function(){
 };
 
 App.prototype.addMessage = function(message) {
-  $('#chats').append($("<div>"+message.text+"</div>"));
+  $('#chats').append($('<p></p>').text(message.text));
 };
 
-//$('form').submit(var messageText = $('#chatBox').value();)
+App.prototype.addRoom = function(roomname) {
+  $('#roomSelect').append($('<option></option>').text(roomname));
+};
 
 var app = new App();
 app.init();
+
+$(document).on('click', '#submitMsg', function(event){
+  event.preventDefault();
+  var message = {
+    username: 'fsociety',
+    text: $('#chatBox').val(),
+    roomname: 'hr38'
+  };
+
+  app.send(message, function(){
+     $('#chatBox').val("");
+  });
+  app.clearMessages();
+  app.init();
+});
